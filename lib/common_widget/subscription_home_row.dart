3b93524e949a74e8ff407cc3f3c59c7e70030503
Fetty <fathifadhil996@gmail.com>
2025-12-19 04:02:55 +0700

    update

--- lib/common_widget/subscription_home_row.dart
+++ lib/common_widget/subscription_home_row.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
-
 import '../common/color_extension.dart';
+import '../common/theme_manager.dart';
 
 class SubScriptionHomeRow extends StatelessWidget {
   final Map sObj;
@@ -11,57 +11,68 @@ class SubScriptionHomeRow extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.only(bottom: 8),
-      child: InkWell(
-        borderRadius:  BorderRadius.circular(16),
-        onTap: onPressed,
-        child: Container(
-          height: 64,
-           padding: const EdgeInsets.all( 10),
-          decoration: BoxDecoration(
-            border: Border.all(
-              color: TColor.border.withOpacity(0.15),
-            ),
-            
+
+    return ValueListenableBuilder<bool>(
+      valueListenable: themeNotifier,
+      builder: (context, isDarkMode, child) {
+
+        var textColor = isDarkMode ? TColor.white : TColor.gray;
+
+
+        var borderColor = isDarkMode
+            ? TColor.border.withOpacity(0.15)
+            : TColor.gray30.withOpacity(0.3);
+
+        return Padding(
+          padding: const EdgeInsets.only(bottom: 8),
+          child: InkWell(
             borderRadius: BorderRadius.circular(16),
-          ),
-          alignment: Alignment.center,
-          child: Row(
-            children: [
-              
-              
-              Image.asset(
-                sObj["icon"],
-                width: 40,
-                height: 40,
-              ),
-              const SizedBox(
-                width: 8,
-              ),
-              Expanded(
-                child: Text(
-                  sObj["name"],
-                  style: TextStyle(
-                      color: TColor.white,
-                      fontSize: 14,
-                      fontWeight: FontWeight.w600),
+            onTap: onPressed,
+            child: Container(
+              height: 64,
+              padding: const EdgeInsets.all(10),
+              decoration: BoxDecoration(
+                border: Border.all(
+                  color: borderColor, 
                 ),
+                borderRadius: BorderRadius.circular(16),
               ),
-              const SizedBox(
-                width: 8,
+              alignment: Alignment.center,
+              child: Row(
+                children: [
+                  Image.asset(
+                    sObj["icon"],
+                    width: 40,
+                    height: 40,
+                  ),
+                  const SizedBox(
+                    width: 8,
+                  ),
+                  Expanded(
+                    child: Text(
+                      sObj["name"],
+                      style: TextStyle(
+                          color: textColor, 
+                          fontSize: 14,
+                          fontWeight: FontWeight.w600),
+                    ),
+                  ),
+                  const SizedBox(
+                    width: 8,
+                  ),
+                  Text(
+                    "Rp${sObj["price"]}",
+                    style: TextStyle(
+                        color: textColor, 
+                        fontSize: 14,
+                        fontWeight: FontWeight.w600),
+                  )
+                ],
               ),
-              Text(
-                "\$${sObj["price"]}",
-                style: TextStyle(
-                    color: TColor.white,
-                    fontSize: 14,
-                    fontWeight: FontWeight.w600),
-              )
-            ],
+            ),
           ),
-        ),
-      ),
+        );
+      },
     );
   }
 }
