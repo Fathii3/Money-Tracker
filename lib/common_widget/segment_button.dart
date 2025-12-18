8ed6223c48dd526eb01eb2d012473bb1e35541c4
Fetty <fathifadhil996@gmail.com>
2025-12-19 04:07:39 +0700

    update

--- lib/common_widget/segment_button.dart
+++ lib/common_widget/segment_button.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
-
 import '../common/color_extension.dart';
+import '../common/theme_manager.dart';
 
 class SegmentButton extends StatelessWidget {
   final String title;
@@ -14,28 +14,40 @@ class SegmentButton extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return InkWell(
-      borderRadius: BorderRadius.circular(12),
-      onTap: onPressed,
-      child: Container(
-        decoration: isActive
-            ? BoxDecoration(
-                border: Border.all(
-                  color: TColor.border.withOpacity(0.15),
-                ),
-                color: TColor.gray60.withOpacity(0.2),
-                borderRadius: BorderRadius.circular(12),
-              )
-            : null,
-        alignment: Alignment.center,
-        child: Text(
-          title,
-          style: TextStyle(
-              color: isActive ? TColor.white : TColor.gray30,
-              fontSize: 12,
-              fontWeight: FontWeight.w600),
-        ),
-      ),
-    );
+    return ValueListenableBuilder<bool>(
+        valueListenable: themeNotifier,
+        builder: (context, isDarkMode, child) {
+          var activeBgColor =
+              isDarkMode ? TColor.gray60.withOpacity(0.2) : TColor.gray;
+
+          var borderColor =
+              isDarkMode ? TColor.border.withOpacity(0.15) : Colors.transparent;
+          var activeTextColor = TColor.white;
+          var inactiveTextColor = isDarkMode ? TColor.gray30 : TColor.gray50;
+
+          return InkWell(
+            borderRadius: BorderRadius.circular(12),
+            onTap: onPressed,
+            child: Container(
+              decoration: isActive
+                  ? BoxDecoration(
+                      border: Border.all(
+                        color: borderColor,
+                      ),
+                      color: activeBgColor,
+                      borderRadius: BorderRadius.circular(12),
+                    )
+                  : null,
+              alignment: Alignment.center,
+              child: Text(
+                title,
+                style: TextStyle(
+                    color: isActive ? activeTextColor : inactiveTextColor,
+                    fontSize: 12,
+                    fontWeight: FontWeight.w600),
+              ),
+            ),
+          );
+        });
   }
 }
