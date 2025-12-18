c6c8c565cab2b22b58ecbb30d8d377f0c5f46786
Fetty <fathifadhil996@gmail.com>
2025-12-19 04:02:06 +0700

    update

--- lib/common_widget/upcoming_bill_row.dart
+++ lib/common_widget/upcoming_bill_row.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
-
 import '../common/color_extension.dart';
+import '../common/theme_manager.dart';
 
 class UpcomingBillRow extends StatelessWidget {
   final Map sObj;
@@ -11,78 +11,95 @@ class UpcomingBillRow extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.only(bottom: 8),
-      child: InkWell(
-        borderRadius: BorderRadius.circular(16),
-        onTap: onPressed,
-        child: Container(
-          height: 64,
-          padding: const EdgeInsets.all(10),
-          decoration: BoxDecoration(
-            border: Border.all(
-              color: TColor.border.withOpacity(0.15),
-            ),
-            borderRadius: BorderRadius.circular(16),
-          ),
-          alignment: Alignment.center,
-          child: Row(
-            children: [
-              Container(
-                height: 40,
-                width: 40,
-                padding: const EdgeInsets.all(4),
-                decoration: BoxDecoration(
-                  color: TColor.gray70.withOpacity(0.5),
-                  borderRadius: BorderRadius.circular(10),
-                ),
-                alignment: Alignment.center,
-                child: Column(
-                  children: [
-                    Text(
-                      "Jun",
-                      style: TextStyle(
-                          color: TColor.gray30,
-                          fontSize: 10,
-                          fontWeight: FontWeight.w500),
-                    ),
 
-                    Text(
-                      "25",
+    return ValueListenableBuilder<bool>(
+      valueListenable: themeNotifier,
+      builder: (context, isDarkMode, child) {
+
+        var textColor = isDarkMode ? TColor.white : TColor.gray;
+        var subTextColor = isDarkMode ? TColor.gray30 : TColor.gray50;
+
+        var dateBoxColor = isDarkMode
+            ? TColor.gray70.withOpacity(0.5)
+            : TColor.gray10.withOpacity(0.5);
+
+        var borderColor = isDarkMode
+            ? TColor.border.withOpacity(0.15)
+            : TColor.gray30.withOpacity(0.3);
+
+        return Padding(
+          padding: const EdgeInsets.only(bottom: 8),
+          child: InkWell(
+            borderRadius: BorderRadius.circular(16),
+            onTap: onPressed,
+            child: Container(
+              height: 64,
+              padding: const EdgeInsets.all(10),
+              decoration: BoxDecoration(
+                border: Border.all(
+                  color: borderColor, 
+                ),
+                borderRadius: BorderRadius.circular(16),
+              ),
+              alignment: Alignment.center,
+              child: Row(
+                children: [
+                  Container(
+                    height: 40,
+                    width: 40,
+                    padding: const EdgeInsets.all(4),
+                    decoration: BoxDecoration(
+                      color: dateBoxColor, 
+                      borderRadius: BorderRadius.circular(10),
+                    ),
+                    alignment: Alignment.center,
+                    child: Column(
+                      children: [
+                        Text(
+                          "Jun",
+                          style: TextStyle(
+                              color: subTextColor, 
+                              fontSize: 10,
+                              fontWeight: FontWeight.w500),
+                        ),
+                        Text(
+                          "25",
+                          style: TextStyle(
+                              color: subTextColor, 
+                              fontSize: 14,
+                              fontWeight: FontWeight.w500),
+                        )
+                      ],
+                    ),
+                  ),
+                  const SizedBox(
+                    width: 8,
+                  ),
+                  Expanded(
+                    child: Text(
+                      sObj["name"],
                       style: TextStyle(
-                          color: TColor.gray30,
+                          color: textColor, 
                           fontSize: 14,
-                          fontWeight: FontWeight.w500),
-                    )
-                  ],
-                ),
+                          fontWeight: FontWeight.w600),
+                    ),
+                  ),
+                  const SizedBox(
+                    width: 8,
+                  ),
+                  Text(
+                    "Rp${sObj["price"]}",
+                    style: TextStyle(
+                        color: textColor, 
+                        fontSize: 14,
+                        fontWeight: FontWeight.w600),
+                  )
+                ],
               ),
-              const SizedBox(
-                width: 8,
-              ),
-              Expanded(
-                child: Text(
-                  sObj["name"],
-                  style: TextStyle(
-                      color: TColor.white,
-                      fontSize: 14,
-                      fontWeight: FontWeight.w600),
-                ),
-              ),
-              const SizedBox(
-                width: 8,
-              ),
-              Text(
-                "\$${sObj["price"]}",
-                style: TextStyle(
-                    color: TColor.white,
-                    fontSize: 14,
-                    fontWeight: FontWeight.w600),
-              )
-            ],
+            ),
           ),
-        ),
-      ),
+        );
+      },
     );
   }
 }
